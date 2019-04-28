cc3d28c7c621b959ecdad623f6f94765e8700dd6
chutiponpon <chutipon.ppn@gmail.com>
2019-04-28 10:58:53 +0700

    add vue-headr for title

--- vue-day2/src/views/BlogDetail.vue
+++ vue-day2/src/views/BlogDetail.vue
@@ -1,5 +1,49 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="blog-detail" v-if="blog.title">
+    <h1>{{ blog.title.rendered }}</h1>
+    <p v-html="blog.excerpt.rendered" class="wrap"></p>
+    Read more...
   </div>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      blog: {},
+      title: ""
+    };
+  },
+  head: {
+    title: function() {
+      return {
+        inner: this.title
+      };
+    }
+  },
+  async mounted() {
+    let res = await axios.get(
+      "https://blog.skooldio.com/wp-json/wp/v2/posts/" + this.$route.params.id
+    );
+    this.blog = res.data;
+    this.title = res.data.title.rendered;
+    this.$emit("updateHead");
+  }
+};
+</script>
+
+<style>
+.blog-detail {
+  width: 80%;
+  margin-left: auto;
+  margin-right: auto;
+}
+.wrap {
+  width: 100%;
+}
+img {
+  max-width: 100%;
+}
+</style>
--- vue-day2/src/views/Home.vue
+++ vue-day2/src/views/Home.vue
@@ -1,20 +1,39 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul>
+      <li v-for="blog in blogs" :key="blog.id">
+        <router-link
+          :to="{
+            name: 'blog-detail',
+            params: {
+              id: blog.id
+            }
+          }"
+          >{{ blog.title.rendered }}</router-link
+        >
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 import axios from "axios";
 // @ is an alias to /src
-
 export default {
-  name: 'home',
+  name: "home",
   data() {
     return {
       blogs: []
     };
   },
-}
+  async mounted() {
+    let res = await axios.get("https://blog.skooldio.com/wp-json/wp/v2/posts/");
+    this.blogs = res.data;
+  }
+};
 </script>
+<style>
+li {
+  list-style: none;
+}
+</style>
